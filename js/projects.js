0121691071c394d06eabef3a30afffc44b4427c4
Rohit Walia <waliar@pointclickcare.com>
2019-02-04 21:19:44 -0500

    update to projects and some other things

--- js/projects.js
+++ js/projects.js
@@ -1,12 +1,36 @@
 
-function showPyramid() {
-  var x = document.getElementById("projectContent");
+function showPyramid()
+{
 
-  if (x.style.display === "none")
+  var other = document.getElementById("automationFramework");
+  other.style.display = "none";
+
+  var element = document.getElementById("dynamicPyramid");
+
+  if (element.style.display === "none")
   {
-    x.style.display = "block";
-  } else
+    element.style.display = "block";
+  }
+  else
   {
-    x.style.display = "none";
+    element.style.display = "none";
+  }
+}
+
+
+function showAutomation()
+{
+  var other = document.getElementById("dynamicPyramid");
+  other.style.display = "none";
+
+  var element = document.getElementById("automationFramework");
+
+  if (element.style.display === "none")
+  {
+    element.style.display = "block";
+  }
+  else
+  {
+    element.style.display = "none";
   }
 }
